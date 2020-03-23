8a55a33ed38ca8b153e24afeedabcc5ff5ade358
hb4y <hugobaymon@gmail.com>
2020-03-23 22:15:28 +0000

    [task 0] create a file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-#!/usr/bin/env bash
+# create a file in /tmp
+file {
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
