97cf1caf33fee950ec72c9a9cf3c2e356c82dfc9
hb4y <hugobaymon@gmail.com>
2020-05-04 23:22:22 +0000

    [task 1] export to cvs

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+comsume API from https://jsonplaceholder.typicode.com
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        api = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+
+        data = requests.get("{}/users/{}".format(api, user_id)).json()
+        todos = requests.get("{}/users/{}/todos".format(api, user_id)).json()
+        done = 0
+        total = len(todos)
+        tasks = []
+        file_name = "{}.csv".format(user_id)
+
+        with open(file_name, mode='w') as employee_file:
+            writer = csv.writer(employee_file, quoting=csv.QUOTE_ALL)
+            name = data.get("name")
+
+            for task in todos:
+                writer.writerow([
+                        user_id,
+                        name,
+                        task.get("completed"),
+                        task.get("title")])
