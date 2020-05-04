8f08d65d12621501fbfa5ae525b81b60a7563def
hb4y <hugobaymon@gmail.com>
2020-05-04 21:48:33 +0000

    [task 0] Get API  response

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+comsume API
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    api = "https://jsonplaceholder.typicode.com"
+    user_id = sys.argv[1]
+
+    user_data = requests.get("{}/users/{}".format(api, user_id)).json()
+    user_todos = requests.get("{}/users/{}/todos".format(api, user_id)).json()
+    done_task = 0
+    total_task = len(user_todos)
+    tasks = []
+
+    for task in user_todos:
+        if task["completed"] is True:
+            done_task += 1
+        tasks.append("\t " + task["title"])
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            user_data["name"], done_task, total_task))
+    print('\n'.join(tasks))
