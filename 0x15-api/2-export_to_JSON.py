c418934a2cc60163bc4ccb320267c6141346f774
hb4y <hugobaymon@gmail.com>
2020-05-05 00:00:10 +0000

    [task 2] to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+comsume API from https://jsonplaceholder.typicode.com
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        api = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+
+        data = requests.get("{}/users/{}".format(api, user_id)).json()
+        todos = requests.get("{}/users/{}/todos".format(api, user_id)).json()
+        tasks = []
+        file_name = "{}.json".format(user_id)
+
+        for task in todos:
+            aux = {}
+            aux['task'] = task.get("title")
+            aux['completed'] = task.get("completed")
+            aux['username'] = data.get("username")
+            tasks.append(aux)
+        to_dict = {user_id: tasks}
+
+        with open(file_name, mode='w') as employee_file:
+            json.dump(to_dict, employee_file)
