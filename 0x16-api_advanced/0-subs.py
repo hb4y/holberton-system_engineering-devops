df8750fe03f82fb2e504812a5f3455d5e069e08b
hb4y <hugobaymon@gmail.com>
2020-05-08 01:15:21 +0000

    [task 0] if subs is null

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,19 @@ get subs for a subreddit
 
 
 def number_of_subscribers(subreddit):
+    """
+        function to get json response
+    """
     api = 'https://www.reddit.com/r/{}/about.json'
 
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     head = {"User-Agent": user_agent}
-    req = requests.get(api.format(subreddit), headers=head).json().get('data')
+    req = requests.get(api.format(subreddit), headers=head).json()
 
     if req:
-        subs = req.get('subscribers')
-        if subs:
-            return subs
+        dat = req.get('data')
+        if dat:
+            subs = dat.get('subscribers')
+            if subs:
+                return subs
     return 0
