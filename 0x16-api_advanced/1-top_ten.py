83532a9cb4ec124394dd83072c1ab8ae17ad5920
hb4y <hugobaymon@gmail.com>
2020-05-08 02:17:37 +0000

    [task 1] get top 10 hot post from subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+get top 10 post of subreddit
+"""
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+        function to get json response
+    """
+    api = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    head = {"User-Agent": user_agent}
+    response = requests.get(api, headers=head)
+
+    if not response:
+        return None
+
+    for i in range(10):
+        dat = response.json().get('data') \
+              .get('children')[i].get('data').get('title')
+        if dat:
+            print(dat)
