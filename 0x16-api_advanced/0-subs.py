8c4fd7338b0d8d49ea8e34c65d87373c41d197c8
hb4y <hugobaymon@gmail.com>
2020-05-08 01:22:49 +0000

    wtf???

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,16 +9,15 @@ def number_of_subscribers(subreddit):
     """
         function to get json response
     """
-    api = 'https://www.reddit.com/r/{}/about.json'
+    api = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     head = {"User-Agent": user_agent}
-    req = requests.get(api.format(subreddit), headers=head).json()
+    req = requests.get(api, headers=head).json()
 
-    if req:
-        dat = req.get('data')
-        if dat:
-            subs = dat.get('subscribers')
-            if subs:
-                return subs
+    dat = req.get('data')
+    if dat:
+        subs = dat.get('subscribers')
+        if subs:
+            return subs
     return 0
