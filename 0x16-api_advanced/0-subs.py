4dc1663202f2004b2a2a1a931da63cbb69eff895
hb4y <hugobaymon@gmail.com>
2020-05-08 00:51:05 +0000

    [task 0] count subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import requests
+"""
+get subs
+"""
+
+
+def number_of_subscribers(subreddit):
+    api = 'https://www.reddit.com/r/{}/about.json'
+
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    head = {"User-Agent": user_agent}
+    req = requests.get(api.format(subreddit), headers=head).json().get('data')
+
+    if req:
+        return req.get('subscribers')
+    return 0
